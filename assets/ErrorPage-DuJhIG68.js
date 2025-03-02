9e6a83d79b8fac9e4a7137f224fc2fdce2abbac2
Dennis Corvers <denniscorvers@hotmail.com>
2025-03-02 15:06:44 +0100

    Updates

---
+++ assets/ErrorPage-DuJhIG68.js
@@ -0,0 +1,25 @@
+import { j as jsxRuntimeExports } from "./index-zRla5ymQ.js";
+const ErrorPage = () => {
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs(
+    "div",
+    {
+      style: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        padding: "20px",
+        textAlign: "center"
+      },
+      children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsx("h1", { children: "Oops, something went wrong!" }),
+        /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "The page you're looking for either doesn't exist or encountered an error while loading." })
+      ]
+    }
+  );
+};
+export {
+  ErrorPage as default
+};
+//# sourceMappingURL=ErrorPage-DuJhIG68.js.map
